afe3fb8017938089a22b6a61a72d1c33f96081a4
Neha <97065146+NehaKasoju@users.noreply.github.com>
2025-05-10 18:50:40 -0400

    js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+} 
